f77e700badd4cf65e68799e915f9601d6e84d1c0
Nikita Vedeneev <nik@quansight.com>
2020-11-27 15:42:18 +0000

    minor

--- aten/src/ATen/native/BatchLinearAlgebra.cpp
+++ aten/src/ATen/native/BatchLinearAlgebra.cpp
@@ -1393,18 +1393,23 @@ static void apply_schur(Tensor& schur_form, Tensor& schur_vectors,
   auto ldvs = lda;
   auto* work_data = at::empty({3 * lda}, schur_form.options()).data_ptr<scalar_t>();
   auto lwork = 3 * lda;
-  auto* rwork_data = at::empty({lda},
-      schur_form.options().dtype(c10::toValueType(schur_form.scalar_type()))
-      ).data_ptr<value_t>();
-  auto bwork = at::empty({lda}, schur_form.options().dtype(at::kInt)).data_ptr<int>();
+  auto* rwork_data = at::empty({lda}, c10::toValueType(schur_form.scalar_type()))
+    .data_ptr<value_t>();
+  auto bwork = at::empty({lda}, at::kInt).data_ptr<int>();
   int info;
 
   // Run once, first to get the optimum work size.
   // Since we deal with batches of matrices with the same dimensions, doing this outside
   // the loop saves (batch_size - 1) workspace queries which would provide the same result
   // and (batch_size - 1) calls to allocate and deallocate workspace using at::empty()
-  //lwork = -1;
-  //scalar_t wkopt;
+ // lwork = -1;
+ // scalar_t wkopt;
+ // lapackGees<scalar_t>(jobvs, sort, select, n,
+ //     schur_form_data, lda, &sdim, wr_data, wi_data,
+ //     schur_vectors_data, ldvs, &wkopt, lwork,
+ //     rwork_data, bwork, &info);
+ // std::cout << "lwork: " << wkopt << std::endl;
+ // std::cout << "lda: " << lda << std::endl;
   lapackGees<scalar_t>(jobvs, sort, select, n,
       schur_form_data, lda, &sdim, wr_data, wi_data,
       schur_vectors_data, ldvs, work_data, lwork,
