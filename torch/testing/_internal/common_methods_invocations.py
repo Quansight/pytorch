a4f0b7fa601c0028653883bc6068db7ab961d8ee
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-03-04 00:00:09 -0800

    [special] add `torch.special` namespace (#52296)
    Summary:
    Reference: https://github.com/pytorch/pytorch/issues/50345
    
     * Add `torch.special` namespace
    * Add `torch.special.gammaln` (alias to `torch.lgamma`)
    
    TODO:
    * Add proper entries for docs.
       * [x] Add .rst file entry
       * [x] Add documentation
       * [x] Update `lgamma` OpInfo entry for alias to `special.gammaln`.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/52296
    
    Reviewed By: ngimel
    
    Differential Revision: D26754890
    
    Pulled By: mruberry
    
    fbshipit-source-id: 73479f68989d6443ad07b7b02763fa98973c15f6

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -2711,6 +2711,7 @@ if TEST_SCIPY:
                        ),
         UnaryUfuncInfo('lgamma',
                        ref=reference_lgamma,
+                       aliases=('special.gammaln', ),
                        decorators=(precisionOverride({torch.float16: 7e-1}),),
                        dtypes=all_types_and(torch.bool),
                        dtypesIfCPU=all_types_and(torch.bool, torch.bfloat16),
